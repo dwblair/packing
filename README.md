407dfd90f2a04d2e9aa625af8a1ec2bc0a5ff8e6
dwblair <donblair@gmail.com>
2013-02-28 13:25:16 -0500

    simultaneous image grab

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ irkit
 =====
 
 Raspberry Pi-based infrared/visible image compositing and webcam control (Public Lab)
+
+* fswebcam must be installed
+* try running "python snap.py" to take 5 snapshots, separated by several seconds ...
---
+++ snap.py
@@ -0,0 +1,38 @@
+import datetime, os, time, argparse, multiprocessing, subprocess
+
+def work(cmd):
+    return subprocess.call(cmd, shell=False)
+
+IR_PATH  = "IR"
+VIS_PATH = "VIS"
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+    if not os.path.isdir(IR_PATH):
+        os.mkdir(IR_PATH)
+    if not os.path.isdir(VIS_PATH):
+        os.mkdir(VIS_PATH)
+    capture_delay = 8
+    capture_num   = 5
+
+    count = multiprocessing.cpu_count()  #get num of multiprocessors
+
+    for i in range(capture_num):
+        dt = datetime.datetime.now()
+        dtstr = dt.strftime("%Y-%m-%d_%H_%M_%S")
+        fn = "%simg%03d.png" % (dtstr,i)
+        fpath0 = os.sep.join((IR_PATH,fn))
+        fpath1 = os.sep.join((VIS_PATH,fn))
+        cmd0 = "fswebcam -r 1280x1024 --png 9 -D 1 -S 3 --save %s -d /dev/video0" % fpath0 
+        cmd1 = "fswebcam -r 1280x1024 --png 9 -D 1 -S 3 --save %s -d /dev/video1" % fpath1 
+        cmd0 = cmd0.split()
+        cmd1 = cmd1.split()
+        pool = multiprocessing.Pool(processes=count)
+        print pool.map(work, [cmd0,cmd1])
+        time.sleep(capture_delay)
+    
+
