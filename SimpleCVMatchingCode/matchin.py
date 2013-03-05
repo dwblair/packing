e064303a9da4421dc2743b99fa718ae1a9e1907a
dwblair <donblair@gmail.com>
2013-03-05 12:04:22 -0500

    test

--- SimpleCVMatchingCode/matchin.py
+++
@@ -1,8 +0,0 @@
-from SimpleCV import Image
-
-for i in range(10):
-    template = Image('wave1.jpg')
-    img = Image('wave2.jpg')
-    match = img.drawKeypointMatches(template,490.00,0.05)
-    match.save("match.jpg")
-    print i
